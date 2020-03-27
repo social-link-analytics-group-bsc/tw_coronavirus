a499c5e1eeb6ca5895534e64f6f71b1a357e56b7
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-03-27 17:52:26 +0100

    Improve performance of the infer_language function

--- src/data_wrangler.py
+++ src/data_wrangler.py
@@ -27,36 +27,25 @@ def infer_language(data_folder, input_file_name, sample=False):
     except IOError:
         pass
 
-    logging.info('Reading file: {}'.format(input_file_name))
-    tweets = []
-    with open(input_file_name, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        row_counter = 0
-        for row in csv_reader:
-            if row['tweet_id'] in processed_tweet_ids:
-                # ignore tweets that were already processed
-                continue
-            row_counter += 1
-            if sample and row_counter > sample_size:
-                break            
-            tweets.append({
-                'tweet_id': row['tweet_id'],
-                'tweet': row['tweet']
-            })    
-
-    logging.info('Starting process of infering languages of tweets...')
-    total_tweets = len(tweets)
-    try:        
-        tweet_counter = 0
-        tweet_langs = []
-        for tweet in tweets:
-            tweet_counter += 1
-            logging.info('[{0}/{1}] Infering language of tweet: {2}'.\
-                         format(tweet_counter, total_tweets, tweet['tweet_id']))
-            lang = detect_language(tweet['tweet'])
-            tweet_langs.append(
-                {'tweet_id': tweet['tweet_id'], 'lang': lang}
-            )         
+    logging.info('Infering language of tweets...')
+    tweet_langs = []
+    try:
+        with open(input_file_name, 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            row_counter = 0
+            for row in csv_reader:            
+                if row['tweet_id'] in processed_tweet_ids:
+                    # ignore tweets that were already processed
+                    continue
+                row_counter += 1
+                if sample and row_counter > sample_size:
+                    break 
+                logging.info('[{0}] Infering language of tweet: {2}'.\
+                            format(row_counter, row['tweet_id']))
+                lang = detect_language(row['tweet'])
+                tweet_langs.append(
+                    {'tweet_id': row['tweet_id'], 'lang': lang}
+                )         
     except Exception as e:
         logging.exception('The following error occurs when infering language '\
                           'of tweets')
