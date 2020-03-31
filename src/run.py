aefd81f6104903710207e54e45c5026233fd3bd9
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-03-31 15:22:00 +0200

    Add two commandas to generate network of users and network of interactions

--- src/run.py
+++ src/run.py
@@ -5,6 +5,7 @@ import pathlib
 import sys
 
 from data_wrangler import infer_language
+from network_analysis import NetworkAnalyzer
 
 # Add the directory to the sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -18,9 +19,19 @@ logging.basicConfig(filename=str(pathlib.Path(__file__).parents[0].joinpath('tw_
 @click.argument('data_dir', type=click.Path(exists=True)) #Path to data directory
 @click.argument('tweets_file') #Name file of the tweets datset
 @click.option('--sample', help='Run task on a sample', default=False, is_flag=True)
-def run_task(detect_language, data_dir, tweets_file, sample):
+@click.option('--create_db_users', help='Create database of users', default=False, is_flag=True)
+@click.argument('tweets_db_name') #Name of the database of tweets
+@click.option('--create_interaction_net', help='Create network of interactions', default=False, is_flag=True)
+def run_task(detect_language, data_dir, tweets_file, sample, create_db_users, \
+             tweets_db_name, create_interaction_net, users_db_name):
     if detect_language:
         infer_language(data_dir, tweets_file, sample)
+    elif create_db_users:
+        na = NetworkAnalyzer(tweets_db_name)
+        na.create_users_db()
+    elif create_interaction_net:
+        na = NetworkAnalyzer()
+        na.generate_network()
     else:
         click.UsageError('Illegal user: Please indicate a running option. ' \
                          'Type --help for more information of the available ' \
