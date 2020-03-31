f5980e71bbb13ae9158f4140812ee3b14b6f498a
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-03-31 17:29:10 +0200

    Improve definition of commands used to generate database of users and network of interactions

--- src/run.py
+++ src/run.py
@@ -14,33 +14,54 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 logging.basicConfig(filename=str(pathlib.Path(__file__).parents[0].joinpath('tw_coronavirus.log')),
                     level=logging.DEBUG)
 
-@click.command()
-@click.option('--detect_language', help='Infer language of tweets', default=False, is_flag=True)
-@click.argument('data_dir', type=click.Path(exists=True)) #Path to data directory
-@click.argument('tweets_file') #Name file of the tweets datset
-@click.option('--sample', help='Run task on a sample', default=False, is_flag=True)
-@click.option('--create_db_users', help='Create database of users', default=False, is_flag=True)
-@click.argument('tweets_db_name') #Name of the database of tweets
-@click.option('--create_interaction_net', help='Create network of interactions', default=False, is_flag=True)
-def run_task(detect_language, data_dir, tweets_file, sample, create_db_users, \
-             tweets_db_name, create_interaction_net, users_db_name):
-    if detect_language:
-        infer_language(data_dir, tweets_file, sample)
-    elif create_db_users:
-        na = NetworkAnalyzer(tweets_db_name)
-        na.create_users_db()
-    elif create_interaction_net:
-        na = NetworkAnalyzer()
-        na.generate_network()
-    else:
-        click.UsageError('Illegal user: Please indicate a running option. ' \
-                         'Type --help for more information of the available ' \
-                         'options.')
 
-
-if __name__ == '__main__':
+def check_current_directory():
     cd_name = os.path.basename(os.getcwd())
     if cd_name != 'src':
         click.UsageError('Illegal use: This script must run from the src directory')
-    else:
-        run_task()
+
+@click.group()
+def run():
+    pass
+
+@run.command()
+@click.argument('data_dir', type=click.Path(exists=True)) #Path to data directory
+@click.argument('tweets_file') #Name file of the tweets datset
+@click.option('--sample', help='Run task on a sample', default=False, is_flag=True)
+def detect_language(data_dir, tweets_file, sample):
+    """
+    Infer language of tweets
+    """
+    check_current_directory()
+    infer_language(data_dir, tweets_file, sample)        
+
+@run.command()
+@click.argument('tweets_collection_name') # Name of collections that contain tweets
+def create_db_users(tweets_collection_name):
+    """
+    Create a database of users that published tweets
+    """
+    check_current_directory()
+    print('Process of creating the database of users has started, please check the ' \
+          'log for updates...')
+    na = NetworkAnalyzer(tweets_collection_name)
+    na.create_users_db()
+    print('Process has finished, results were stored in the collection users in ' \
+          'your database.')
+    
+@run.command()
+def create_interaction_net():
+    """
+    Create a network of users interactions
+    """
+    check_current_directory()
+    print('Process of creating the network of interactions has started, please check the ' \
+          'log for updates...')
+    na = NetworkAnalyzer()
+    na.generate_network()
+    print('Process has finished, results were stored in the collection networks in ' \
+          'your database.')
+
+
+if __name__ == "__main__":
+    run()
