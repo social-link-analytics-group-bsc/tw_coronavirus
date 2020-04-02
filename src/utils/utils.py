5b6cceb0daa01f79404c10a4bfdcd28ad4f16eaf
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-04-02 19:18:59 +0200

    Add function to compute the datetime of tweets

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,17 @@
 import json
 
+from datetime import datetime
+
 # Get configuration from file
 def get_config(config_file):
     with open(str(config_file), 'r') as f:
         config = json.loads(f.read())
     return config
+
+
+def get_tweet_datetime(tw_creation_dt):
+    dt_obj = datetime.strptime(tw_creation_dt, '%a %b %d %H:%M:%S %z %Y')        
+    tw_date_time = dt_obj.strftime("%m/%d/%Y, %H:%M:%S")
+    tw_date = dt_obj.strftime("%m/%d/%Y")
+    tw_time = dt_obj.strftime("%H:%M:%S")
+    return tw_date_time, tw_date, tw_time
