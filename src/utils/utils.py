f2e53ca2142918196c227fdf43852ccd389c99ef
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-04-03 01:02:16 +0200

    Add a function that calculates the remaining execution time

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,13 @@
 import json
+import logging
+import pathlib
+import time
 
-from datetime import datetime
+from datetime import datetime, timedelta
+
+logging.basicConfig(filename=str(pathlib.Path(__file__).parents[1].joinpath('tw_coronavirus.log')),
+                    level=logging.DEBUG)
+
 
 # Get configuration from file
 def get_config(config_file):
@@ -15,3 +22,15 @@ def get_tweet_datetime(tw_creation_dt):
     tw_date = dt_obj.strftime("%m/%d/%Y")
     tw_time = dt_obj.strftime("%H:%M:%S")
     return tw_date_time, tw_date, tw_time
+
+
+def calculate_remaining_execution_time(start_time, total_segs, 
+                                       processing_records, total_records):
+    end_time = time.time()
+    total_segs += end_time - start_time
+    remaining_secs = (total_segs/processing_records) * \
+                     (total_records - processing_records)
+    remaining_time = str(timedelta(seconds=remaining_secs))
+    logging.info('Time remaining to process all tweets: ' \
+                 '{} to complete.'.format(remaining_time))
+    return total_segs
