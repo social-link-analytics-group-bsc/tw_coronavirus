3e0eec64e4bc47bb2da91ec39e0aad94b460c1bb
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-04-04 17:05:43 +0200

    Add method to upload information of tweets to the remote server

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,7 +6,8 @@ import time
 
 from datetime import timedelta
 from utils.db_manager import DBManager
-from utils.utils import calculate_remaining_execution_time
+from utils.utils import calculate_remaining_execution_time, SPAIN_LANGUAGES, \
+                        get_spain_places_regex
 
 logging.basicConfig(filename=str(pathlib.Path(__file__).parents[0].joinpath('tw_coronavirus.log')),
                     level=logging.DEBUG)
@@ -25,21 +26,21 @@ def insert_tweets_from_csv():
                     dbm.insert_tweet(row)
 
 
-def upload_tweets():
+def upload_tweet_sentiment():
+    print('Process started, it can take several time. Follow updates through ' \
+          'the log...')
     dbm_local = DBManager(collection='tweets_esp_hpai')
     dbm_remote = DBManager(collection='bsc-ls', 
-                           config_fn='src/config_mongo_hpai.json')
-    tweets = dbm_local.search({'$and': [
-                                        {'lang': {'$in': ['es', 'ca', 'eu', 'gl']}},
-                                        {'$or': [{"place.country": "Spain"}, 
-                                                 {'user.location': {'$ne': None}}
-                                                ]},
-                                        {'retweeted_status': {'$exists': 0}},
-                                        {'sentiment_score': {'$exists': 1}}
-                                        ]
-                               })
+                           config_fn='config_mongo_hpai.json')
+    query = {'$and': [{'lang': {'$in': SPAIN_LANGUAGES}},
+                      {'$or': [{'place.country': 'Spain'}, 
+                               {'user.location': {'$in': \
+                                   get_spain_places_regex()}}]}]}
+    query.update({'retweeted_status': {'$exists': 0}})
+    query.update({'sentiment_score': {'$exists': 1}})
+    tweets = dbm_local.search(query)
     total_tweets = tweets.count()
-    processing_counter = total_segs = 0
+    processing_counter = total_segs = modified_records = found_tweets = 0
     logging.info('Going to upload {0:,} tweets'.format(total_tweets))
     for tweet in tweets:
         start_time = time.time()
@@ -47,16 +48,36 @@ def upload_tweets():
         logging.info('[{0}/{1}] Processing tweet:\n{2}'.\
                      format(processing_counter, total_tweets, tweet['id']))
         sentiment_dict = {
-            'sentiment_score': tweet['sentiment_score'],
-            'sentiment_score_polyglot': tweet['sentiment_score_polyglot'],
-            'sentiment_score_sentipy': tweet['sentiment_score_sentipy'],
-            'sentiment_score_affin': tweet['sentiment_score_affin']
+            'sentiment': {
+                'score': tweet['sentiment_score'] if tweet['sentiment_score_polyglot'] else None,                
+            }            
         }
-        ret_update = dbm_remote.update_record({'id': tweet['id']}, sentiment_dict)
-        if ret_update.matched_count > 0:
-            logging.info('Remote tweet update with sentiment info')
-        else:
-            logging.info('Could not find remote tweet')
+        if tweet['sentiment_score_polyglot']:
+            sentiment_dict['sentiment']['raw_score_polyglot'] = \
+                tweet['sentiment_score_polyglot']
+        if 'sentiment_score_sentipy' in tweet:
+            sentiment_dict['sentiment']['raw_score_sentipy'] = \
+                tweet['sentiment_score_sentipy']
+        if 'sentiment_score_affin' in tweet:
+            sentiment_dict['sentiment']['raw_score_affin'] = \
+                tweet['sentiment_score_affin']
+        ret_update = dbm_remote.update_record({'id': int(tweet['id'])}, 
+                                               sentiment_dict)
+        if ret_update.matched_count == 0:
+            logging.info('Could not find in the remote server a tweet with ' \
+                         'the id {}'.format(tweet['id']))
+        elif ret_update.matched_count == 1:
+            found_tweets += 1
+            if ret_update.modified_count == 0:
+                logging.info('Found tweet but did not update.')
+            elif ret_update.modified_count == 1:
+                modified_records += 1
+                logging.info('Remote tweet update with sentiment info!')
         total_segs += calculate_remaining_execution_time(start_time, total_segs, 
                                                          processing_counter, 
                                                          total_tweets)
+    logging.info('Total processed tweets: {0:,}\n'\
+                 'Total found tweets in remote server: {1:,}\n'
+                 'Total updated tweets in remote server: {2:,}\n'.\
+                 format(total_tweets, found_tweets, modified_records))
+    print('Process finished!')
