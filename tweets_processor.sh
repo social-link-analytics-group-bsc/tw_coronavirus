94ee5394bc7c5f816828715cec49334a5ef26f95
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-04-24 15:36:58 +0200

    Add new calls

--- tweets_processor.sh
+++ tweets_processor.sh
@@ -11,8 +11,18 @@ fi
 
 LOGFILE=${PROJECT_DIR}/tweets_downloader.log
 ERRORFILE=${PROJECT_DIR}/tweets_downloader.err
+EVENT_LOG=${PROJECT_DIR}/process_events_log.csv
+COLLECTION_NAME='processed'
+CONFIG_FILE_NAME='config_mongo_inb.json'
 error=0
 
+####
+# Print a starting message
+####
+running_date=`date '+%Y-%m-%d'`
+printf "\n\n#####\nStarting to process tweets at ${running_date}\n######\n\n" >> $LOGFILE
+start_time=`date '+%Y-%m-%d %H:%M:%S'`
+echo "${running_date},'starting_processor',${start_time}," >> $EVENT_LOG
 
 ####
 # Go to project directory
@@ -37,8 +47,10 @@ fi
 if [ $? -eq 0 ]
 then
     cd src
-    echo "[1/2] Running sentiment analysis..."
-    python run.py preprocess 'processed' --config_file 'config_mongo_inb.json' > $LOGFILE 2> $ERRORFILE
+    echo "[1/4] Running sentiment analysis..."
+    start_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'analyzing_sentiments',${start_time}," >> $EVENT_LOG
+    python run.py preprocess $COLLECTION_NAME --config_file $CONFIG_FILE_NAME >> $LOGFILE 2>> $ERRORFILE    
 else
     error=1
 fi
@@ -49,13 +61,59 @@ fi
 ####
 if [ $? -eq 0 ]
 then
-    echo "[2/2] Running language detection..."
-    python run.py add-language-flag 'processed' --config_file 'config_mongo_inb.json' > $LOGFILE 2> $ERRORFILE
+    end_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'analyzing_sentiments',,${end_time}" >> $EVENT_LOG
+    echo "[2/4] Running language detection..."
+    start_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'detecting_languages',${start_time}," >> $EVENT_LOG
+    python run.py add-language-flag $COLLECTION_NAME --config_file $CONFIG_FILE_NAME >> $LOGFILE 2>> $ERRORFILE    
+else
+    error=1
+fi
+
+####
+# Add Spain location flags
+####
+if [ $? -eq 0 ]
+then
+    end_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'detecting_languages',,${end_time}" >> $EVENT_LOG
+    echo "[3/4] Adding Spain location flags..."
+    start_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'adding_locations',${start_time}," >> $EVENT_LOG
+    python run.py add-location-flags $COLLECTION_NAME --config_file $CONFIG_FILE_NAME >> $LOGFILE 2>> $ERRORFILE
+else
+    error=1
+fi
+
+
+####
+# Add query version flag
+####
+if [ $? -eq 0 ]
+then
+    end_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'adding_locations',,${end_time}" >> $EVENT_LOG
+    echo "[4/4] Adding query version flag..."
+    start_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'adding_query_versions',${start_time}," >> $EVENT_LOG
+    python run.py add-query-version-flag $COLLECTION_NAME --config_file $CONFIG_FILE_NAME >> $LOGFILE 2>> $ERRORFILE
 else
     error=1
 fi
 
 if [ $? -eq 0 ]
+then
+    end_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'adding_query_versions',,${end_time}" >> $EVENT_LOG
+    end_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'finished_processor',,${end_time}" >> $EVENT_LOG
+else
+    error=1
+fi
+
+
+if [ $error -eq 0 ]
 then
     echo "Process has finished successfully"
     echo "For more information, check $LOGFILE"
