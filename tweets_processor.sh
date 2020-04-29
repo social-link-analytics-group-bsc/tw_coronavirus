f9187e7bfb3fa150f4a23dcd582ffc558531feea
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-04-29 14:49:33 +0200

    Fix a bug in the command that initializes conda

--- tweets_processor.sh
+++ tweets_processor.sh
@@ -48,8 +48,8 @@ then
     then
         source env/bin/activate
     else
-        `conda init bash`
-        `conda activate ${CONDA_ENV}`
+        source /opt/conda/bin/conda
+        `conda activate $CONDA_ENV`
     fi
 else
     error=1
