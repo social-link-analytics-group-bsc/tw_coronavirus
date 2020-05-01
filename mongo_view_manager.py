2d3d84a76949f44a9420b16be4c49134d37a5cef
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-05-01 16:35:43 +0200

    Change import to absolute import

--- mongo_view_manager.py
+++ mongo_view_manager.py
@@ -1,15 +1,7 @@
-import os
-import sys
-
-# Add the directory to the sys.path
-path_current_file = os.path.dirname(os.path.abspath(__file__))
-path_utils = os.path.join(path_current_file, 'src', 'utils')
-sys.path.append(os.path.dirname(path_utils))
-
 from datetime import datetime
-from utils.db_manager import DBManager
 
 import click
+from src.utils.db_manager import DBManager
 
 
 @click.group()
