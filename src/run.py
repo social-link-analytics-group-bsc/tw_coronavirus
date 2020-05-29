617d3e4ab5015d559d293531cbd6a69930981dec
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-05-29 12:06:18 +0200

    Add a command to add tweet type flag

--- src/run.py
+++ src/run.py
@@ -12,7 +12,7 @@ from data_wrangler import infer_language, add_date_time_field_tweet_objs, \
       add_covid_keywords_flag, add_lang_flag, add_place_flag, sentiment_evaluation, \
       update_sentiment_score_fields, do_drop_collection, do_add_language_flag, \
       add_esp_location_flags, do_add_query_version_flag, update_metric_tweets, \
-      do_add_complete_text_flag
+      do_add_complete_text_flag, do_add_tweet_type_flag
 from data_loader import upload_tweet_sentiment, do_collection_merging, \
      do_update_collection
 from network_analysis import NetworkAnalyzer
@@ -244,6 +244,19 @@ def extract_user_sample(sample_size, collection_name, config_file, output_file):
     check_current_directory()
     print('Extracting sample of users')
     export_user_sample(sample_size, collection_name, config_file, output_file)
+
+
+@run.command()
+@click.argument('collection_name') # Name of collections that contain tweets
+@click.option('--config_file', help='File with Mongo configuration', \
+              default=None, is_flag=False)
+def add_tweet_type_flag(collection_name, config_file):
+    """
+    Add tweet type flag
+    """
+    check_current_directory()
+    print('Adding tweet type flag')
+    do_add_tweet_type_flag(collection_name, config_file)
 
 
 if __name__ == "__main__":
