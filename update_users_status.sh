53bb2dc485f97e9f04f6ad6400a0aad2a60bd68c
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-06-19 16:03:15 +0200

    Update script used to update the collection of users

--- update_users_status.sh
+++ update_users_status.sh
@@ -13,14 +13,14 @@ EVENT_LOG=${LOG_DIR}/process_events_log.csv
 ENV_DIR="${PROJECT_DIR}/env"
 CONFIG_FILE_NAME='config_mongo_inb.json'
 CONDA_ENV='twcovid'
-NUM_TASKS=1
+NUM_TASKS=2
 error=0
 
 ####
 # Print a starting message
 ####
 running_date=`date '+%Y-%m-%d'`
-printf "\n\n#####\nStarting to update users status at ${running_date}\n######\n\n" >> $LOGFILE
+printf "\n\n#####\nStarting to update users' collection at ${running_date}\n######\n\n" >> $LOGFILE
 start_time=`date '+%Y-%m-%d %H:%M:%S'`
 echo "${running_date},'starting_processor',${start_time}," >> $EVENT_LOG
 
@@ -50,18 +50,35 @@ fi
 echo "Updating collection ${COLLECTION_NAME}..."
 
 ####
-# Update status of users
+# Augment users' data
 ####
 if [[ $? -eq 0 ]] && [[ $error -eq 0 ]]
 then
     cd src
-    echo "[1/${NUM_TASKS}] Updating collection of users..."
+    echo "[1/${NUM_TASKS}] Augmenting users' data..."
+    start_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'augmeting_data',${start_time}," >> $EVENT_LOG
+    python run.py augment-user-data users $COLLECTION_NAME --config_file $CONFIG_FILE_NAME >> $LOGFILE 2>> $ERRORFILE
+else
+    error=1
+fi
+
+####
+# Update status of users
+####
+if [[ $? -eq 0 ]] && [[ $error -eq 0 ]]
+then
+    end_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'augmeting_data',,${end_time}" >> $EVENT_LOG
+    echo "[2/${NUM_TASKS}] Updating status of users..."
     start_time=`date '+%Y-%m-%d %H:%M:%S'`
     echo "${running_date},'updating_status',${start_time}," >> $EVENT_LOG
     python run.py update-user-status users $COLLECTION_NAME --config_file $CONFIG_FILE_NAME >> $LOGFILE 2>> $ERRORFILE
 else
     error=1
 fi
+
+
 
 if [[ $? -eq 0 ]] && [[ $error -eq 0 ]]
 then
