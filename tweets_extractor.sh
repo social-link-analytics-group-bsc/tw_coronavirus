a57df9b6179d966cd44ed577d686bb8612e0f50e
Jorge Saldivar <jorgesaldivar@gmail.com>
2020-09-04 16:59:22 +0200

    Extend script to include indexing and learning of embeddings

--- tweets_extractor.sh
+++ tweets_extractor.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 PROJECT_DIR=`pwd`
-USER_COLLECTION='users'
 COLLECTION_NAME='processed_new'
 
 for arg in "$@"
@@ -14,6 +13,18 @@ do
         --collection_name=*)
         COLLECTION_NAME="${arg#*=}"
         shift # Remove --collection_name= from processing
+        ;;
+        --destination_dir=*)
+        DESTINATION_DIR="${arg#*=}"
+        shift # Remove --destination_dir= from processing
+        ;;
+        --lingo4g_dir=*)
+        LINGO4G_DIR="${arg#*=}"
+        shift # Remove --lingo4g_dir= from processing
+        ;;
+        --lingo4g_project_file=*)
+        LINGO4G_PROJECT_FILE="${arg#*=}"
+        shift # Remove --lingo4g_project_file= from processing
         ;;
     esac
 done
@@ -33,7 +44,7 @@ OUTPUT_DIR="${PROJECT_DIR}/data"
 OUTPUT_FILE="${OUTPUT_DIR}/tweets.json"
 CONFIG_FILE_NAME='config_mongo_inb.json'
 CONDA_ENV='twcovid'
-NUM_TASKS=1
+NUM_TASKS=4
 error=0
 
 ####
@@ -67,26 +78,63 @@ else
     error=1
 fi
 
-echo "Updating collection ${COLLECTION_NAME}..."
+echo "Exporting tweets from the collection ${COLLECTION_NAME}..."
 
 ####
 # Extract tweet to JSON
 ####
+# if [[ $? -eq 0 ]] && [[ $error -eq 0 ]]
+# then
+#     cd src
+#     echo "[1/${NUM_TASKS}] Exporting tweets..."
+#     start_time=`date '+%Y-%m-%d %H:%M:%S'`
+#     echo "${running_date},${COLLECTION_NAME},'export_tweets',${start_time}," >> $EVENT_LOG
+#     python run.py export-tweets $COLLECTION_NAME $OUTPUT_FILE --config_file $CONFIG_FILE_NAME --lang 'es' >> $LOGFILE 2>> $ERRORFILE
+# else
+#     error=1
+# fi
+
+# if [[ $? -eq 0 ]] && [[ $error -eq 0 ]]
+# then
+#     end_time=`date '+%Y-%m-%d %H:%M:%S'`
+#     echo "${running_date},'export_tweets',,${end_time}" >> $EVENT_LOG
+#     echo "[2/${NUM_TASKS}] Copying output file to the destination directory..."
+#     start_time=`date '+%Y-%m-%d %H:%M:%S'`
+#     echo "${running_date},${COLLECTION_NAME},'copy_file',${start_time}," >> $EVENT_LOG
+#     cp $OUTPUT_FILE $DESTINATION_DIR >> $LOGFILE 2>> $ERRORFILE
+# else
+#     error=1
+# fi
+
 if [[ $? -eq 0 ]] && [[ $error -eq 0 ]]
 then
-    cd src
-    echo "[1/${NUM_TASKS}] Adding tweet type flag..."
-    start_time=`date '+%Y-%m-%d %H:%M:%S'`
-    echo "${running_date},${COLLECTION_NAME},'add_type_flag',${start_time}," >> $EVENT_LOG
-    python run.py export-tweets $COLLECTION_NAME $OUTPUT_FILE --config_file $CONFIG_FILE_NAME --lang 'es' >> $LOGFILE 2>> $ERRORFILE
+    end_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'copy_file',,${end_time}" >> $EVENT_LOG
+    cd $LINGO4G_DIR
+    echo "[3/${NUM_TASKS}] Indexing new tweets..."
+    start_time=`date '+%Y-%m-%d %H:%M:%S'`    
+    echo "${running_date},${COLLECTION_NAME},'index_tweets',${start_time}," >> $EVENT_LOG
+    l4g index -p ${LINGO4G_PROJECT_FILE} --force >> $LOGFILE 2>> $ERRORFILE    
 else
     error=1
 fi
 
 if [[ $? -eq 0 ]] && [[ $error -eq 0 ]]
-then    
+then
     end_time=`date '+%Y-%m-%d %H:%M:%S'`
-    echo "${running_date},'finished_processor',,${end_time}" >> $EVENT_LOG
+    echo "${running_date},'index_tweets',,${end_time}" >> $EVENT_LOG
+    echo "[4/${NUM_TASKS}] Learning embeddings..."
+    start_time=`date '+%Y-%m-%d %H:%M:%S'`    
+    echo "${running_date},${COLLECTION_NAME},'learn_embeddings',${start_time}," >> $EVENT_LOG
+    l4g learn-embeddings -p ${LINGO4G_PROJECT_FILE} >> $LOGFILE 2>> $ERRORFILE
+else
+    error=1
+fi
+
+if [[ $? -eq 0 ]] && [[ $error -eq 0 ]]
+then
+    end_time=`date '+%Y-%m-%d %H:%M:%S'`
+    echo "${running_date},'learn_embeddings',,${end_time}" >> $EVENT_LOG    
 else
     error=1
 fi
